cafd04217e3a90b3e0ccedb0c8c1842c29849343
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2023-02-18 15:37:31 +0100

    Add files via upload

---
+++ Tp-Inversion-Of-Control-And-Dependency-Injection/src/main/java/com/taffah/Dao/IDaoImpl.java
@@ -0,0 +1,12 @@
+package com.taffah.Dao;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+@Component
+public class IDaoImpl implements IDao{
+    @Override
+    public Date getDate() {
+        return new Date();
+    }
+}
