489ac0013f553aaf87196e8bda1d1f77c1a98c79
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2023-02-26 15:43:16 +0100

    Add files via upload

---
+++ JPA-APP/src/main/java/com/example/jpaapp/JpaAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.jpaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAppApplication.class, args);
+    }
+}
